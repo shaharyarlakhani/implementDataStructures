59789a660e5d3fc4ce28f5ea500e20f4e79f8185
shaharyarlakhani <shaharyar.lakhani@credera.com>
2020-07-17 17:00:25 -0500

    finished basic arraylist class and added tester

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,15 +1,24 @@
+import javax.xml.bind.annotation.XmlElement.DEFAULT;
+
 public class MyArrayList {
 
     private Object[] arr;
     private int size;
 
+    private static final int DEFAULT_CAP = 10;
+
     public MyArrayList() {
-        this.arr = new Object[10];
+        this.arr = new Object[DEFAULT_CAP];
         this.size = 0;
     }
 
     public void add(Object o) {
         insert(size, o);
+    }
+
+    // pre: 0 <= idx < size
+    public Object get(int idx) {
+        return arr[idx];
     }
 
     // pre 0 <= idx < size
@@ -33,10 +42,45 @@ public class MyArrayList {
         size++;
     }
 
+    public int size() {
+        return size;
+    }
+
     private void resize() {
         Object[] temp = new Object[arr.length * 2 + 1];
         System.arraycopy(arr, 0, temp, 0, size);
         arr = temp;
+    }
+
+    // uses StringBuffer
+    public String toString() {
+        StringBuffer toRet = new StringBuffer();
+        toRet.append("[");
+        if (size > 0) {
+            toRet.append(arr[0]);
+            for (int i = 1; i < size; i++) {
+                toRet.append(", " + arr[i]);
+            }
+        }
+        toRet.append("]");
+        return toRet.toString();
+    }
+
+    public boolean equals(Object other) {
+        if (other == null)
+            return false;
+        if (other == this)
+            return true;
+        if (other.getClass() != this.getClass())
+            return false;
+        MyArrayList otherList = (MyArrayList) other;
+        boolean result = size == otherList.size();
+        int i = 0;
+        while (i < size && result) {
+            result = arr[i].equals(otherList.get(i));
+            i++;
+        }
+        return result;
     }
 
 }
---
+++ MyArrayListTester.java
@@ -0,0 +1,13 @@
+public class MyArrayListTester {
+
+    public static void main(String[] args) {
+        MyArrayList a1 = new MyArrayList();
+        MyArrayList a2 = new MyArrayList();
+
+        a1.add(1);
+        a2.add(2);
+
+        System.out.println(a1.toString());
+        System.out.println(a2.toString());
+    }
+}
